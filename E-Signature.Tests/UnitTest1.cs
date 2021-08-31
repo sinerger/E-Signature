7cd66189c7ddf31282419cbb3efa2f9bddff855c
Ola <oleksandrakondratenko@gmail.com>
2021-08-31 11:49:03 +0300

    CreateSing

--- E-Signature.Tests/UnitTest1.cs
+++ E-Signature.Tests/UnitTest1.cs
@@ -1,28 +1,74 @@
 using System;
+using System.Collections;
 using NUnit.Framework;
+using FluentAssertions;
 
 namespace E_Signature.Tests
 {
     public class Tests
     {
-        [Test]
-        public void GetSing_WhenValidTestPassed_ShouldReturnSing(string actualJSON, string secretKey, string expectedJson)
+        [TestCaseSource(typeof(GetDataForTestGetSing))]
+        public void GetSing_WhenValidTestPassed_ShouldNotHaveSameSing(string actualJSON, string secretKey, string expectedJson)
         {
-            //var actualSing = Signature.GetSing(actualJSON,secretKey);
+            Signature.Configure(TimeSpan.FromMilliseconds(50));
 
-            //var expectedSing = Signature.GetSing(expectedJson, secretKey);
+            var actualSing = Signature.GetSing(actualJSON,secretKey);
 
-            Assert.AreEqual(expectedJson, actualJSON);
+            var expectedSing = Signature.GetSing(expectedJson, secretKey);
+
+            actualSing.Should().NotBe(expectedSing);
         }
 
-        [Test]
+        public class GetDataForTestGetSing : IEnumerable
+        {
+            private string actualJSON = "{\"name\":\"john\",\"age\":22,\"class\":\"mca\"}";
+            private string secretKey = "Very secrete key";
+            private string expectedJson = "{\"name\":\"john\",\"age\":22,\"class\":\"mca\"}";
+
+            public IEnumerator GetEnumerator()
+            {
+
+
+                yield return new object[]
+                {
+                    actualJSON,
+                    secretKey,
+                    expectedJson
+                };
+
+
+            }
+        }
+        public class GetDataForTestGetSing2 : IEnumerable
+        {
+            private string actualJSON = "{\"name\":\"john\",\"age\":22,\"class\":\"mca\"}";
+            private string secretKey = "Very secrete key";
+            private string expectedJson = "{\"name\":\"john\",\"age\":22,\"class\":\"mca\"}";
+
+            public IEnumerator GetEnumerator()
+            {
+
+
+                yield return new object[]
+                {
+                    actualJSON,
+                    secretKey,
+                    TimeSpan.FromMilliseconds(1),
+                    true
+                };
+
+
+            }
+        }
+        [TestCaseSource(typeof(GetDataForTestGetSing2))]
         public void IsValidSing_WhenValidTestPassed_ShouldReturnBool(string inputJson, string secretKey, TimeSpan timeDrift, bool expected)
         {
-            //var inputSing = Signature.GetSing(inputJson,secretKey);
+            Signature.Configure(TimeSpan.FromMilliseconds(10));
+            var inputSing = Signature.GetSing(inputJson, secretKey);
+            
+            var actual = Signature.IsValidSing(inputJson, inputSing, secretKey, timeDrift);
 
-            //var actual = Signature.IsValidSing(inputJson, inputSing, secretKey, timeDrift);
-
-            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
---
+++ E-Signature/Signature.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using E_Signature.Extensions;
+
+namespace E_Signature
+{
+    public static class Signature
+    {
+        private static TimeSpan _timeRounding;
+
+        static Signature()
+        {
+            _timeRounding = TimeSpan.FromSeconds(5);
+        }
+
+        public static void Configure(TimeSpan timeRounding)
+        {
+            _timeRounding = timeRounding;
+        }
+
+        public static string GetSing(string inputBody, string secretKey)
+        {
+            var result = GetSing(inputBody, secretKey, DateTime.Now);
+
+            return result.ToString();
+        }
+
+        public static bool IsValidSing(string inputBody, string inputSing, string secretKey, TimeSpan timeDrift)
+        {
+            if (!string.IsNullOrEmpty(inputBody) && !string.IsNullOrEmpty(secretKey) && !string.IsNullOrEmpty(inputSing) && timeDrift != default)
+            {
+                var startTime = DateTime.Now - timeDrift;
+                var finishTime = DateTime.Now + timeDrift;
+                var result = false;
+
+                var count = 0;
+                for (var i = startTime; i < finishTime; i += _timeRounding)
+                {
+                    count++;
+                    var sing = GetSing(inputBody, secretKey, i);
+
+                    if (sing == inputSing)
+                    {
+                        result = true;
+
+                        break;
+                    }
+                }
+
+                return true;
+            }
+
+            throw new ArgumentException();
+        }
+
+        private static string GetSing(string inputBody, string secretKey, DateTime time)
+        {
+            if (!string.IsNullOrEmpty(inputBody) && !string.IsNullOrEmpty(secretKey))
+            {
+                var timeNow = time.Round(_timeRounding);
+                var currentTime = timeNow.ToString("yyyy.MM.dd HH:mm:ss:ffff");
+                var sing = inputBody.GetHashCode() + secretKey.GetHashCode() + currentTime.GetHashCode();
+
+                return sing.ToString();
+            }
+            else if (string.IsNullOrEmpty(inputBody))
+            {
+                throw new ArgumentNullException("String inputBody is empty or null");
+            }
+            else
+            {
+                throw new ArgumentNullException("String secretKey is empty or null");
+            }
+
+        }
+    }
+}
