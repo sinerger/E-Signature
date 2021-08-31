2be63e4715b83aac365b24e0fc4eaffd1cf904ea
sinerger <skochmen@gmail.com>
2021-08-31 10:25:20 +0300

    Init Test project

--- E-Signature.Tests/UnitTest1.cs
+++ E-Signature.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace E_Signature.Tests
@@ -5,9 +6,23 @@ namespace E_Signature.Tests
     public class Tests
     {
         [Test]
-        public void Test1(string actualJSON, string secretKey, string expectedJson)
+        public void GetSing_WhenValidTestPassed_ShouldReturnSing(string actualJSON, string secretKey, string expectedJson)
         {
-            //SomeClass.GetSing(actualJSON,secretKey)
+            //var actualSing = Signature.GetSing(actualJSON,secretKey);
+
+            //var expectedSing = Signature.GetSing(expectedJson, secretKey);
+
+            Assert.AreEqual(expectedJson, actualJSON);
+        }
+
+        [Test]
+        public void IsValidSing_WhenValidTestPassed_ShouldReturnBool(string inputJson, string secretKey, TimeSpan timeDrift, bool expected)
+        {
+            //var inputSing = Signature.GetSing(inputJson,secretKey);
+
+            //var actual = Signature.IsValidSing(inputJson, inputSing, secretKey, timeDrift);
+
+            //Assert.AreEqual(expected, actual);
         }
     }
 }
