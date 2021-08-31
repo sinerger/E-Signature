4700eb7235521ac06d2c3194dd2f5029dc583452
sinerger <skochmen@gmail.com>
2021-08-31 10:16:56 +0300

    Add project files.

---
+++ E-Signature.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace E_Signature.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1(string actualJSON, string secretKey, string expectedJson)
+        {
+            //SomeClass.GetSing(actualJSON,secretKey)
+        }
+    }
+}
